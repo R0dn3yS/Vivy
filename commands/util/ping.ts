1bb47d32c92789c02187ee9abecd1fdcc3b97ab3
Rodney van den Velden <rodneymichael2002@gmail.com>
2021-11-18 01:03:24 +0100

    Made ping working

--- commands/util/ping.ts
+++ commands/util/ping.ts
@@ -1,8 +1,10 @@
 import { CommandContext, Command} from '../../deps.ts';
 
 export default class PingCommand extends Command {
-	name = 'ping'
+	name = 'ping';
 	execute(ctx: CommandContext) {
-		ctx.message.reply('Pong!');
+		ctx.message.reply('pinging').then(m => {
+			m.edit(`Latency is ${ctx.client.gateway.ping}ms`)
+		});
 	}
 }
--- mod.ts
+++ mod.ts
@@ -2,7 +2,7 @@ import { CommandClient, Intents } from './deps.ts';
 import { config } from './config.ts';
 
 const client = new CommandClient({
-	prefix: '!'
+	prefix: '!',
 });
 
 client.on('ready', () => {
