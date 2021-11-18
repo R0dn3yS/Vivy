677a38e39ea26acd8033dbd92b95292acfa6a3e9
Rodney van den Velden <rodneymichael2002@gmail.com>
2021-11-18 02:19:10 +0100

    Added category

--- commands/util/ping.ts
+++ commands/util/ping.ts
@@ -2,6 +2,7 @@ import { CommandContext, Command} from '../../deps.ts';
 
 export default class PingCommand extends Command {
 	name = 'ping';
+	category = 'util';
 	execute(ctx: CommandContext) {
 		ctx.message.reply('pinging').then(m => {
 			m.edit(`Latency is ${ctx.client.gateway.ping}ms`)
