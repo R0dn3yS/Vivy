6c59070e6a0169694d2f7f495c527208272f0d3b
Rodney van den Velden <rodneymichael2002@gmail.com>
2021-11-18 16:17:55 +0100

    Added userinfo command

---
+++ commands/information/userinfo.ts
@@ -0,0 +1,51 @@
+import { CommandContext, Command, Embed, stripIndent } from '../../deps.ts';
+import type { Args, User, Member } from '../../deps.ts';
+
+export default class UserinfoCommand extends Command {
+	name = 'userinfo';
+	aliases = ['ui'];
+	category = 'information';
+	args: Args[] = [
+		{
+			name: 'user',
+			match: 'user',
+		},
+	]
+	async execute(ctx: CommandContext) {
+		// Get User
+		let cUser: User
+		if (!ctx.args!.user) {
+			cUser = ctx.author
+		} else {
+			cUser = ctx.args!.user as User;
+		}
+
+		const cMember = await ctx.guild!.members.resolve(cUser.id) as Member;
+
+		// Get Roles
+		const roles: string[] = [];
+		(await cMember.roles.array()).forEach(role => {
+			roles.push(`<@&${role.id}>`)
+		})
+
+		// Get Dates
+		const created = Intl.DateTimeFormat('en-US').format(new Date(cUser.timestamp))
+		const joined = Intl.DateTimeFormat('en-US').format(new Date(cMember.joinedAt))
+		
+		const uEmbed = new Embed()
+			.setFooter(cMember.displayName, cUser.avatarURL())
+			.setThumbnail(cUser.avatarURL())
+			.setColor(await cMember.effectiveColor())
+			.addField('Member Information', stripIndent `**> Display Name:** ${cMember.displayName}
+			**> Joined At:** ${joined}
+			**> Roles:** ${roles.join(' ')}`, true)
+			.addField('User Information', stripIndent `**> ID:** ${cUser.id}
+			**> Username:** ${cUser.username}
+			**> Discord Tag:** ${cUser.tag}
+			**> Created At** ${created}`, true)
+			.setTimestamp(Date.now())
+
+		ctx.message.channel.send(uEmbed);
+
+	}
+}
