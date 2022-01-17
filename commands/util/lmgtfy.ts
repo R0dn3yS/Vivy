23f30322ca0f179d0025eac2f9f24c6b7b7006fc
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-01-17 13:51:41 +0100

    Added LMGTFY

---
+++ commands/util/lmgtfy.ts
@@ -0,0 +1,18 @@
+import { CommandContext, Command } from '../../deps.ts';
+import type { Args } from '../../deps.ts';
+
+export default class EightballCommand extends Command {
+    name = 'lmgtfy';
+    category = 'util';
+    args: Args[] = [
+        {
+            name: 'query',
+            match: 'rest',
+        },
+    ]
+    execute(ctx: CommandContext) {
+      const query = ctx.args!.query as string
+
+      return ctx.channel.send('https://lmgtfy.app/?q=' + query.split(' ').join('+'));
+    }
+}
