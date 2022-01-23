edfbb5cdf902f80046b612acc29ebd6f0b994e3b
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-01-23 23:52:58 +0100

    use Deno from upstream branch

--- deps.ts
+++ deps.ts
@@ -1,5 +1,4 @@
-// export * from 'https://code.harmony.rocks/v2.5.1/mod.ts';
-export * from 'https://raw.githubusercontent.com/MierenManz/harmony/parser_refactor/mod.ts'
+export * from 'https://code.harmony.rocks/main/mod.ts';
 
 // @deno-types="https://deno.land/x/usefultags@1.1.1/usefulTags.d.ts"
 export { stripAllIndents as stripIndent } from "https://deno.land/x/usefultags@1.1.1/usefulTags.mjs";
