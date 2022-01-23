1c9f51b6b69df62a3f5a4e91288c1a62386e1280
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-01-23 16:16:47 +0100

    Removed lavalink from deps

--- deps.ts
+++ deps.ts
@@ -1,7 +1,5 @@
 // export * from 'https://code.harmony.rocks/v2.5.1/mod.ts';
 export * from 'https://raw.githubusercontent.com/MierenManz/harmony/parser_refactor/mod.ts'
-export * as lava from 'https://deno.land/x/lavadeno@3.2.2/mod.ts';
-export { Lavalink } from "https://deno.land/x/lavadeno@3.2.2/deps.ts";
 
 // @deno-types="https://deno.land/x/usefultags@1.1.1/usefulTags.d.ts"
 export { stripAllIndents as stripIndent } from "https://deno.land/x/usefultags@1.1.1/usefulTags.mjs";
