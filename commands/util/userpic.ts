fc9439cf065b9d1479fa55afd1cb1473583fa22a
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-01-24 17:04:16 +0100

    Add userpic command

---
+++ commands/util/userpic.ts
@@ -0,0 +1,25 @@
+import { Command, CommandContext } from '../../deps.ts';
+import type { Args, User } from '../../deps.ts';
+
+export default class UserPictureCommand extends Command {
+  name = 'userpicture';
+  aliases = ['userpic'];
+  category = 'util';
+  optionalArgs = true;
+  args: Args[] = [
+    {
+      name: 'user',
+      match: 'user',
+    },
+  ];
+  execute(ctx: CommandContext) {
+    let user: User;
+    if (!ctx.args!.user) {
+      user = ctx.author;
+    } else {
+      user = ctx.args!.user as User;
+    }
+
+    ctx.channel.send(user.avatarURL());
+  }
+}
